66d5b35cea932dc9beb2024b1a6f59d888873594
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2023-12-18 13:14:45 -0500

    Scrolling background code

--- Background.java
+++ Background.java
@@ -23,3 +23,32 @@ public class Background extends Actor
     }
 
 }
+
+// public class Background extends Actor
+// {
+    // private int imageWidth;
+    // private int offset = 100;
+    // private Background otherBackground;
+    // private int speed = 4;
+    
+    // public Background() {
+        // imageWidth = getImage().getWidth();    
+    // }
+    
+    // public void setOtherBackground(Background otherBackground) {
+        // this.otherBackground = otherBackground;
+    // }
+    
+    // public void increaseSpeed() {
+        // speed += 1;
+    // }
+    
+    // public void act()
+    // {
+        // if(getX() < -imageWidth + offset) {
+            // int newX = otherBackground.getX() + imageWidth;
+            // setLocation(newX, getY());
+        // }
+        // move(-speed);
+    // }
+// }
--- MyWorld.java
+++ MyWorld.java
@@ -51,3 +51,22 @@ public class MyWorld extends World
     
     }
 }
+
+
+// public class ScrollingWorld extends World
+// {
+    // Background bg1 = new Background();
+    // Background bg2 = new Background();
+    
+    // public ScrollingWorld()
+    // {    
+        // super(600, 400, 1, false); 
+
+        // bg1.setOtherBackground(bg2);
+        // bg2.setOtherBackground(bg1);
+
+        // addObject(bg1, 0, 200);
+        // addObject(bg2, 1024, 200);
+    // }
+
+// }
